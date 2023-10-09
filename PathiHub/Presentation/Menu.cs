40e64d8f5e2ad1f1afb0a1648cda393062ef7f8b
Gavin Renardus <Gavinrenardus@gmail.com>
2023-10-09 22:09:07 +0200

    Added a Loading timer on the start Menu

--- PathiHub/Presentation/Menu.cs
+++ PathiHub/Presentation/Menu.cs
@@ -3,7 +3,7 @@ static class Menu
     // dit is het scherm is word puur gebruikt als eerste scherm
     static public void Start()
     {
-        
+
 
         Console.WriteLine(@"
  ____          __    __              __  __           __        
@@ -13,23 +13,48 @@ static class Menu
   \ \ \/\ \L\.\_\ \ \_\ \ \ \ \ \ \   \ \ \ \ \ \ \_\ \\ \ \L\ \
    \ \_\ \__/.\_\\ \__\\ \_\ \_\ \_\   \ \_\ \_\ \____/ \ \_,__/
     \/_/\/__/\/_/ \/__/ \/_/\/_/\/_/    \/_/\/_/\/___/   \/___/ ");
-        
+
         Console.WriteLine("--------------------------------------------------------------------------------");
         Console.WriteLine();
-        Console.WriteLine("Uitgelichte films:");
+        Console.WriteLine("featured films:");
         Console.WriteLine();
-        
-        
+
         //print elke promoted movie van list
         Console.WriteLine("The Avengers");
         Console.WriteLine("Movie test 2");
         Console.WriteLine("Five Guys 3");
+
+        Console.WriteLine("--------------------------------------------------------------------------------");
+        Console.WriteLine("Press enter to continue");
         
-        Thread.Sleep(5000);
-        Console.Clear();
-        UserLogin.Start();
         
+        
+
+        ConsoleKeyInfo keyPressed = Console.ReadKey();
+        if (keyPressed.Key == ConsoleKey.Enter)
+        {
+            
+            Console.Write("Loading");
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(500); // Wacht 500 milliseconden (0,5 seconden)
+                Console.Write(".");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Loading Completed.");
+            Thread.Sleep(1000);
+            
+            Console.Clear();
+            UserLogin.Start();
+        }
+        
+        else
+        {
+            Console.WriteLine("Invalid key. Please try again.");
+
+
         }
     }
+}
 
     
