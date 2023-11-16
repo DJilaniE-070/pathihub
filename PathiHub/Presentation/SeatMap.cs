b4260f705369864345871d2c0cef42b20a9773d8
Yong <yongjiang2001@gmail.com>
2023-11-16 22:42:21 +0100

    test
    test

--- PathiHub/Presentation/SeatMap.cs
+++ PathiHub/Presentation/SeatMap.cs
@@ -42,6 +42,8 @@ public class SeatMap
         List<string> row14 = new List<string> { "X", "X", "C", "C", "C", "C", "C", "C", "C", "C", "X", "X" };
         
         List<List<string>> auditorium = new List<List<string>> { row1, row2, row3, row4, row5, row6, row7, row8, row9, row10, row11, row12, row13, row14 };
+
+        
     
         foreach (List<string> row in auditorium)
         {
