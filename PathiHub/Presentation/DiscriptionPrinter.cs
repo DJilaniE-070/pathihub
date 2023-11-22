e529c6700312eb8ff0ba05bfb0dd5e3d9694addc
Gavin Renardus <Gavinrenardus@gmail.com>
2023-11-22 10:18:16 +0100

    First DiscriptionPrinter.cs is made and extra movies edit

--- PathiHub/Presentation/DiscriptionPrinter.cs
+++ PathiHub/Presentation/DiscriptionPrinter.cs
@@ -1,32 +1,71 @@
-using Microsoft.VisualBasic.CompilerServices;
-
 public class DiscriptionPrinter
 {
     public static void DrawBox(Movie chosenmovie)
     {
-        MoviesAccess access= new();
+        MoviesAccess access = new();
         if (access.LoadFromJson() == true)
         {
             List<Movie> movies = access.GetItemList();
             string? selectedmovie = chosenmovie.Plot;
-            
-            
-            // List<Movie> movies = access.GetItemList();
-            // string? plot = movieData.Plot;
-            string whiteLine = "|";
-            Console.WriteLine("+-----------------------------------------------------------------------------+"); 
-            Console.WriteLine("|                                                                             |");
-            Console.WriteLine("|                                                                             |"); 
-            Console.WriteLine("|                                                                             |"); 
-            Console.WriteLine($"{whiteLine}{selectedmovie}{whiteLine}");
-            Console.WriteLine("|                                                                             |");
-            Console.WriteLine("|                                                                             |");
-            Console.WriteLine("|                                                                             |");
-            Console.WriteLine("|                                                                             |");
-            Console.WriteLine("|                                                                             |");
-            Console.WriteLine("+-----------------------------------------------------------------------------+");
-            
+
+            // Define the width of the box
+            int boxWidth = 30;
+
+            // Split the plot into lines
+            List<string> plotLines = WrapText(selectedmovie, boxWidth - 4);
+
+            // Create the top border of the box
+            Console.WriteLine("+" + new string('-', boxWidth - 2) + "+");
+
+            // Create empty lines in the box
+            for (int i = 0; i < (boxWidth - 6) / 2; i++)
+            {
+                Console.WriteLine("|" + new string(' ', boxWidth - 2) + "|");
+            }
+
+            // Print the plot lines within the box
+            foreach (string line in plotLines)
+            {
+                Console.WriteLine($"| {line.PadRight(boxWidth - 6)} |");
+            }
+
+            // Create empty lines in the box
+            for (int i = 0; i < (boxWidth - 6) / 2; i++)
+            {
+                Console.WriteLine("|" + new string(' ', boxWidth - 2) + "|");
+            }
+
+            // Create the bottom border of the box
+            Console.WriteLine("+" + new string('-', boxWidth - 2) + "+");
         }
-        
+    }
+
+    // Function to wrap text into lines of a specified length
+    private static List<string> WrapText(string text, int lineLength)
+    {
+        List<string> lines = new List<string>();
+
+        string[] words = text.Split(' ');
+
+        string currentLine = "";
+        foreach (string word in words)
+        {
+            if ((currentLine + word).Length <= lineLength)
+            {
+                currentLine += word + " ";
+            }
+            else
+            {
+                lines.Add(currentLine.Trim());
+                currentLine = word + " ";
+            }
+        }
+
+        if (!string.IsNullOrWhiteSpace(currentLine))
+        {
+            lines.Add(currentLine.Trim());
+        }
+
+        return lines;
     }
 }
